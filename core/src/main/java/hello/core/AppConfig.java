6a55d2fd2d2f597a01bd8ec18f70d504180c0331
cathy <cathy02473500@gmail.com>
2024-09-06 19:02:28 +0900

    spring core basic section 4

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,40 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    //메소드명에서 역할과 구현체가 드러나게 리팩토링
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    //인터페이스와 구현체 둘 다에 의존하고 있음
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //인터페이스와 구현체 둘 다에 의존하고 있음 -> DIP를 지키도록 변경!
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
